1ecfc15e173e8910f5642f6103f58a1997ce95e3
DmitryK777 <kislyakovdmv@gmail.com>
2024-03-24 12:38:21 +0300

    Transfer parameters by value

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -2,10 +2,38 @@
 #include <iostream>
 using namespace std;
 
+#define POINTERS_BASICS
+//#define POINTERS_AND_ARRAYS
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+#if defined POINTERS_BASICS
 	int a = 2;
 	int* pa = &a;
+	cout << a << endl; // вывод значения переменной 'а'
+	cout << &a << endl; // взятие адреса переменной а прямо при выводе
+	cout << pa << endl;
+	cout << *pa << endl;
+	cout << typeid(pa).name() << endl;
+	cout << pa[0] << endl;
+#endif
+
+#if defined POINTERS_AND_ARRAYS
+
+	const int n = 6;
+	int arr[n] = { 3, 5, 8, 13, 21 };
+	cout << arr << endl;
+	cout << &arr[0] << endl;
+
+	cout << *arr << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+
+#endif
+
 }
